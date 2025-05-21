1a36b9609d619d1e48e9e365064bacd2d1bd30be
Skylar <118846397+NobleSkye@users.noreply.github.com>
2025-05-21 16:28:18 -0700

    add DatapackListCommand to handle datapack listing add gamemodemenu command updated to 1.21.4

---
+++ src/main/java/me/pilkeysek/skyeNetP/commands/DatapackListCommand.java
@@ -0,0 +1,23 @@
+package me.pilkeysek.skyeNetP.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.Bukkit;
+
+public class DatapackList implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("datapacklist")) {
+            if (sender.hasPermission("skyenet.command.datapack.list")) {
+                Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "datapack list");
+                sender.sendMessage("Listing all datapacks...");
+            } else {
+                sender.sendMessage("You do not have permission to use this command.");
+            }
+            return true;
+        }
+        return false;
+    }
+}
